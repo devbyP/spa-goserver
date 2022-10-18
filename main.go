6e436f3e7a1238dd9d368361961e590ea73797b1
Thanapon Teerakaisri <dev.thanapon@gmail.com>
2022-10-19 03:19:32 +0700

    use server port

--- main.go
+++ main.go
@@ -58,10 +58,11 @@ func main() {
 
 	spa := spaHandler{staticPath: "build", indexPath: "index.html"}
 	router.PathPrefix("/").Handler(spa)
+	port := os.Getenv("PORT")
 
 	srv := &http.Server{
 		Handler: router,
-		Addr:    "127.0.0.1:8000",
+		Addr:    ":" + port,
 		// Good practice: enforce timeouts for servers you create!
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
